e34d4fde7480e75540d4f55e1074d62f8d74226f
Borith-Monorum <phanith.vatanak@gmail.com>
2023-03-07 17:35:28 +0700

    update

--- backend/mysite/settings.py
+++ backend/mysite/settings.py
@@ -84,10 +84,21 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'TctKONRFdOydIpQRA0XT',
+        'HOST': 'containers-us-west-145.railway.app',
+        'PORT': '5890',
     }
 }
 
