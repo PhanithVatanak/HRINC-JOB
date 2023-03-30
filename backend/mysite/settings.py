3d75d630506f1e29396f1c220b44c685c0f82fb3
Borith-Monorum <phanith.vatanak@gmail.com>
2023-03-30 09:24:08 +0700

    update

--- backend/mysite/settings.py
+++ backend/mysite/settings.py
@@ -84,23 +84,23 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'railway',
-        'USER': 'postgres',
-        'PASSWORD': 'TctKONRFdOydIpQRA0XT',
-        'HOST': 'containers-us-west-145.railway.app',
-        'PORT': '5890',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'railway',
+#         'USER': 'postgres',
+#         'PASSWORD': 'TctKONRFdOydIpQRA0XT',
+#         'HOST': 'containers-us-west-145.railway.app',
+#         'PORT': '5890',
+#     }
+# }
 
 
 # Password validation
